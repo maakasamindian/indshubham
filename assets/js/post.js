90ca794833e3d3f17dd889595f7fc28e1e1f7441
maakasamindian <74304156+maakasamindian@users.noreply.github.com>
2022-03-13 19:23:16 +0530

    Add files via upload

--- assets/js/post.js
+++ assets/js/post.js
@@ -90,17 +90,32 @@ function getdata(){
           "<div class='post-img-holder'>"+
               "<embed class='post-img image' src='"+value.imageURL+"'>"+
           "</div>"+
+          "<a class='post-download' href='"+value.imageURL+"' download='ind-shubham@uagalaxy'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 512 512'><path d='M336,176h40a40,40,0,0,1,40,40V424a40,40,0,0,1-40,40H136a40,40,0,0,1-40-40V216a40,40,0,0,1,40-40h40' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><polyline points='176 272 256 352 336 272' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><line x1='256' y1='48' x2='256' y2='336' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/></svg></a>"+
           "<div class='post-about'>"+
               "<span class='post-title'>"+value.title+"</span>"+
               "<p class='post-desc'>"+value.desc+"</p>"+
-              "<span class='post-date'>"+value.date+"</span>"+
+          "</div>"+
+          "<div class='post-user'>"+
+                "<img class='post-user-img image' src='assets/img/shubham.jpg' alt='ind.shubham'>"+
+                "<span class='user-name-date'>"+
+                   "<a href='index.html' class='post-user-name'>ind.shubham</a>"+
+                   "<span class='post-date'>"+value.date+"</span>"+
+                "</span>"+
           "</div>"+
           "<button class='post-delete-btn hide' id='"+key+"' onclick='delete_post(this.id)'>Delete <svg xmlns='http://www.w3.org/2000/svg' width='20px' height='20px' viewBox='0 0 512 512'><rect x='32' y='48' width='448' height='80' rx='32' ry='32'/><path d='M74.45,160a8,8,0,0,0-8,8.83L92.76,421.39a1.5,1.5,0,0,0,0,.22A48,48,0,0,0,140.45,464H371.54a48,48,0,0,0,47.67-42.39l0-.21,26.27-252.57a8,8,0,0,0-8-8.83ZM323.31,340.69a16,16,0,1,1-22.63,22.62L256,318.63l-44.69,44.68a16,16,0,0,1-22.63-22.62L233.37,296l-44.69-44.69a16,16,0,0,1,22.63-22.62L256,273.37l44.68-44.68a16,16,0,0,1,22.63,22.62L278.62,296Z'/></svg></button>"+
       "</div>"+posts_div.innerHTML;
       }
+
+   firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        document.querySelectorAll(".post-delete-btn").forEach((bgs)=>{bgs.classList.remove("hide")})
+    }
+    })
+
+/*=========================================================*/
      document.querySelectorAll(".post-img-holder").forEach((e) => {
     let bgimg = e.querySelector("embed").getAttribute("src")
-    e.querySelector("embed").setAttribute("style", "backdrop-filter: blur(10px);")
+    e.querySelector("embed").setAttribute("style", "backdrop-filter: blur(10px) brightness(90%);")
     e.setAttribute("style","background:url("+bgimg+");background-size: contain;");
      return this;  }); 
 
@@ -123,12 +138,9 @@ document.querySelector(".close").onclick = ()=>{
     previewBox.classList.add('hide');
     previewBox.classList.remove('show');
 } 
-    firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        document.querySelectorAll(".post-delete-btn").forEach((bgs)=>{bgs.classList.remove("hide")})
-    }
-    })
+/*===========================================*/
 });
+
 }
 
 function delete_post(key){
